cddff120f6db8d891a31120ba65d0f7d2b1f86d3
misskip <anita.desanmiguel@gmail.com>
2023-02-01 22:38:34 +1100

    add files

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,42 @@
 import "./App.css";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  // Route
+} from "react-router-dom";
+import Home from "./pages/Home" 
+import Login from "./pages/Login" 
+import Register from "./pages/Register" 
+import Single from "./pages/Single"
+import Write from "./pages/Write"  
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>
+      <Navbar />
+      <Home />
+      <Footer />
+    </div>,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
 
 function App() {
   return (
-  
-  <div>hey</div>
+  <div>
+    <RouterProvider router={router} />
+  </div>
   )}
 
 export default App;
---
+++ client/src/pages/Write.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Write = () => {
+  return (
+    <div>Write</div>
+  )
+}
+export default Write
